5e0f79625a49683081b6e9d92f799945dedaf1c7
alduron <rodell@digitalinjections.com>
2018-08-11 01:09:47 -0500

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,141 +1,140 @@
 #Start Loop
-Do{
-    $CurrentVersion = "1.2"
-    $Continue = $True
+
+$CurrentVersion = "1.3"
+$Continue = $True
+
+##LoopClear
+Remove-Variable CanDownload -Force -ErrorAction SilentlyContinue
+Remove-Variable CanShowPhoto -Force -ErrorAction SilentlyContinue
+Remove-Variable Links -Force -ErrorAction SilentlyContinue
+Remove-Variable Link -Force -ErrorAction SilentlyContinue
+Remove-Variable FileTypesO -Force -ErrorAction SilentlyContinue
+Remove-Variable PhotoLinks -Force -ErrorAction SilentlyContinue
+Remove-Variable VideoLinks -Force -ErrorAction SilentlyContinue
+Remove-Variable WebLinks -Force -ErrorAction SilentlyContinue
+Remove-Variable RandPool -Force -ErrorAction SilentlyContinue
+Remove-Variable UpdatePath -Force -ErrorAction SilentlyContinue
+Remove-Variable MediaPathO -Force -ErrorAction SilentlyContinue
+Remove-Variable Sleep -Force -ErrorAction SilentlyContinue
+Remove-Variable Kill -Force -ErrorAction SilentlyContinue
+Remove-Variable NewVersion -Force -ErrorAction SilentlyContinue
+
+$MediaPath = "C:\Users\aldur\Pictures\Thumbs"
+
+##WEBREQUEST
+$Web = Invoke-webrequest http://digitalinjections.com/stinatroll/
+$Variables = ($Web.ParsedHtml.body.getElementsByClassName('entry-content'))[0].innertext -split "`r`n"
+
+##PROCESS WEB
+Foreach ($Var in $Variables){
+    $Name = $Var.split('=',2)[0]
+    $Data = $Var.split('=',2)[1]
     
-    ##LoopClear
-    Remove-Variable CanDownload -Force -ErrorAction SilentlyContinue
-    Remove-Variable CanShowPhoto -Force -ErrorAction SilentlyContinue
-    Remove-Variable Links -Force -ErrorAction SilentlyContinue
-    Remove-Variable Link -Force -ErrorAction SilentlyContinue
-    Remove-Variable FileTypesO -Force -ErrorAction SilentlyContinue
-    Remove-Variable PhotoLinks -Force -ErrorAction SilentlyContinue
-    Remove-Variable VideoLinks -Force -ErrorAction SilentlyContinue
-    Remove-Variable WebLinks -Force -ErrorAction SilentlyContinue
-    Remove-Variable RandPool -Force -ErrorAction SilentlyContinue
-    Remove-Variable UpdatePath -Force -ErrorAction SilentlyContinue
-    Remove-Variable MediaPathO -Force -ErrorAction SilentlyContinue
-    Remove-Variable Sleep -Force -ErrorAction SilentlyContinue
-    Remove-Variable Kill -Force -ErrorAction SilentlyContinue
-    Remove-Variable NewVersion -Force -ErrorAction SilentlyContinue
-    
-    $MediaPath = "C:\Users\aldur\Pictures\Thumbs"
-    
-    ##WEBREQUEST
-    $Web = Invoke-webrequest http://digitalinjections.com/stinatroll/
-    $Variables = ($Web.ParsedHtml.body.getElementsByClassName('entry-content'))[0].innertext -split "`r`n"
-    
-    ##PROCESS WEB
-    Foreach ($Var in $Variables){
-        $Name = $Var.split('=',2)[0]
-        $Data = $Var.split('=',2)[1]
-        
-        if(($Data -match " ") -and ($Name -notmatch "Path")){
-            Write-Host "$Data matched Array"
-            $Array = $Data -split ' +(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)'
-            New-Variable -Name $Name -Value $Array
-        } elseif($Data -match 'True') {
-            Write-Host "True"
-            Invoke-Expression ("`$$($Name) = `$True")
-        } elseif($Data -match 'False') {
-            Write-Host "False"
-            Invoke-Expression ("`$$($Name) = `$False")
-        } else {
-            Write-Host "Default"
-            New-Variable -Name $Name -Value $Data
-        } 
-    }
-    
-    ##CHECK FOR OVERRIDES
-    if($MediaPathO){$MediaPath = $MediaPathO}
-    if($FileTypesO){$FileTypes = $FileTypesO}
-    
-    ##DEFAULTS
-    $WC = New-Object System.Net.WebClient
-    
-    $PhotoFolder = "$MediaRoot\Photos"
-    $VideoFolder = "$MediaRoot\Video"
-    $UpdateFolder = "$MediaRoot\UpdateFile"
-    
-    ##PROCESS DOWNLOADS
-    if($CanDownload){
-        New-Item -ItemType Directory -Force -Path "$PhotoFolder" | Out-Null
-        New-Item -ItemType Directory -Force -Path "$VideoFolder" | Out-Null
-        foreach ($Link in $PhotoLinks){
-            $SystemName = Split-Path $Link -Leaf
-            $Path = "$PhotoFolder\$SystemName"
-            if(!(Get-ChildItem -Path $Path -ErrorAction SilentlyContinue)){
-                $WC.DownloadFile($Link,$Path)
-            }
-        }
-    
-        foreach($Link in $VideoLinks){
-            $SystemName = Split-Path $Link -Leaf
-            $Path = "$VideoFolder\$SystemName"
-            if(!(Get-ChildItem -Path $Path -ErrorAction SilentlyContinue)){
-                $WC.DownloadFile($Link,$Path)
-            }
-        }
-    }
-    
-    ##PROCESS Doables
-    $DoOn = 1,3,5,7,9
-    $DoingIndex = Get-Random -Minimum 1 -Maximum $RandPool
-    Write-Host "DO $DoingIndex"
-    if($DoOn -contains $DoingIndex){
-        Write-Host "Doing"
-        $Index = Get-Random -Maximum 3
-        Write-Host "INDEX $Index"
-    
-        switch($Index){
-            0 {
-                if($CanShowPhoto){
-                    Write-Host "Photos"
-                    $Pictures = Get-ChildItem -Path $PhotoFolder | ?{$FileTypes -match $_.Extension}
-                    $Rand = Get-Random -Minimum 0 -Maximum ($Pictures.Count - 1)
-                    $Picture = $Pictures[$Rand].FullName
-                    & "$Picture"
-                }
-              }
-            1 {
-                if($CanShowVideo){
-                    Write-Host "Video"
-                    $Videos = Get-ChildItem -Path $VideoFolder | ?{$FileTypes -match $_.Extension}
-                    $Rand = Get-Random -Minimum 0 -Maximum $Videos.Count
-                    $Video = $Videos[$Rand].FullName
-                    & "$Video"
-                }
-              }
-            2 {
-                if($CanShowWeb){
-                    Write-Host "Website"
-                    $Rand = Get-Random -Minimum 0 -Maximum $WebLinks.Count
-                    $Website = $WebLinks[$Rand]
-                    Start-Process -FilePath $Website  
-                }
-              }
-        }
+    if(($Data -match " ") -and ($Name -notmatch "Path")){
+        Write-Host "$Data matched Array"
+        $Array = $Data -split ' +(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)'
+        New-Variable -Name $Name -Value $Array
+    } elseif($Data -match 'True') {
+        Write-Host "True"
+        Invoke-Expression ("`$$($Name) = `$True")
+    } elseif($Data -match 'False') {
+        Write-Host "False"
+        Invoke-Expression ("`$$($Name) = `$False")
     } else {
-        Write-Host "Not Doing"
+        Write-Host "Default"
+        New-Variable -Name $Name -Value $Data
+    } 
+}
+
+##CHECK FOR OVERRIDES
+if($MediaPathO){$MediaPath = $MediaPathO}
+if($FileTypesO){$FileTypes = $FileTypesO}
+
+##DEFAULTS
+$WC = New-Object System.Net.WebClient
+
+$PhotoFolder = "$MediaRoot\Photos"
+$VideoFolder = "$MediaRoot\Video"
+$UpdateFolder = "$MediaRoot\UpdateFile"
+
+##PROCESS DOWNLOADS
+if($CanDownload){
+    New-Item -ItemType Directory -Force -Path "$PhotoFolder" | Out-Null
+    New-Item -ItemType Directory -Force -Path "$VideoFolder" | Out-Null
+    foreach ($Link in $PhotoLinks){
+        $SystemName = Split-Path $Link -Leaf
+        $Path = "$PhotoFolder\$SystemName"
+        if(!(Get-ChildItem -Path $Path -ErrorAction SilentlyContinue)){
+            $WC.DownloadFile($Link,$Path)
+        }
+    }
+
+    foreach($Link in $VideoLinks){
+        $SystemName = Split-Path $Link -Leaf
+        $Path = "$VideoFolder\$SystemName"
+        if(!(Get-ChildItem -Path $Path -ErrorAction SilentlyContinue)){
+            $WC.DownloadFile($Link,$Path)
+        }
+    }
+}
+
+##PROCESS Doables
+$DoOn = 1,3,5,7,9
+$DoingIndex = Get-Random -Minimum 1 -Maximum $RandPool
+Write-Host "DO $DoingIndex"
+if($DoOn -contains $DoingIndex){
+    Write-Host "Doing"
+    $Index = Get-Random -Maximum 3
+    Write-Host "INDEX $Index"
+
+    switch($Index){
+        0 {
+            if($CanShowPhoto){
+                Write-Host "Photos"
+                $Pictures = Get-ChildItem -Path $PhotoFolder | ?{$FileTypes -match $_.Extension}
+                $Rand = Get-Random -Minimum 0 -Maximum ($Pictures.Count - 1)
+                $Picture = $Pictures[$Rand].FullName
+                & "$Picture"
+            }
+          }
+        1 {
+            if($CanShowVideo){
+                Write-Host "Video"
+                $Videos = Get-ChildItem -Path $VideoFolder | ?{$FileTypes -match $_.Extension}
+                $Rand = Get-Random -Minimum 0 -Maximum $Videos.Count
+                $Video = $Videos[$Rand].FullName
+                & "$Video"
+            }
+          }
+        2 {
+            if($CanShowWeb){
+                Write-Host "Website"
+                $Rand = Get-Random -Minimum 0 -Maximum $WebLinks.Count
+                $Website = $WebLinks[$Rand]
+                Start-Process -FilePath $Website  
+            }
+          }
+    }
+} else {
+    Write-Host "Not Doing"
+}
+
+##PROCESS UPDATE
+If($CanUpdate){
+    $Script = $MyInvocation.InvocationName
+    $Name = Split-Path $Script -Leaf
+    $TempFile = "$env:TEMP\$Name"
+    Remove-Item $TempFile -Force
+    $WC.DownloadFile($UpdatePath,$TempFile)
+    if($CurrentVersion -notmatch $NewVersion){
+        Write-Host "Copying..."
+        Copy-Item -Path $TempFile -Destination $Script -Force    
     }
     
-    ##PROCESS UPDATE
-    If($CanUpdate){
-        $Script = $MyInvocation.InvocationName
-        $Name = Split-Path $Script -Leaf
-        $TempFile = "$env:TEMP\$Name"
-        Remove-Item $TempFile -Force
-        $WC.DownloadFile($UpdatePath,$TempFile)
-        if($CurrentVersion -notmatch $NewVersion){
-            Write-Host "Copying..."
-            Copy-Item -Path $TempFile -Destination $Script -Force    
-        }
-        
-    }
+}
 
-    ##SAFETY KILL
-    if($Kill){$Continue = $false}
+##WAIT
+Sleep -Seconds $Sleep
 
-    ##WAIT
-    Sleep -Seconds $Sleep
-} while($Continue)
+##SAFETY KILL
+if(!$Kill){Start-ScheduledTask -TaskName Test}
