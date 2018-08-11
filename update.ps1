4b250c6b9d5e7f8de50247994b3e2515e5c3fbfa
alduron <rodell@digitalinjections.com>
2018-08-11 14:37:10 -0500

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,6 +1,6 @@
 Do{
     #Start Loop
-    #TestNo 15
+    #TestNo 16
     
     ##LoopClear
     Remove-Variable CanDownload -Force -ErrorAction SilentlyContinue
@@ -23,7 +23,7 @@ Do{
     Remove-Variable CurrentVersion -Force -ErrorAction SilentlyContinue
 	Remove-Variable Debug -Force -ErrorAction SilentlyContinue
     
-    $CurrentVersion = "1.15"
+    $CurrentVersion = "1.16"
     $Continue = $True
     $MediaPath = "C:\Users\aldur\Pictures\Thumbs"
     
