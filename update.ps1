07fd9a947661bc1d7389ba77241b967851a9403d
alduron <rodell@digitalinjections.com>
2018-08-11 14:02:15 -0500

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -36,17 +36,13 @@ Do{
         $Data = $Var.split('=',2)[1]
         
         if(($Data -match " ") -and ($Name -notmatch "Path")){
-            Write-Host "$Data matched Array"
             $Array = $Data -split ' +(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)'
             New-Variable -Name $Name -Value $Array
         } elseif($Data -match 'True') {
-            Write-Host "True"
             Invoke-Expression ("`$$($Name) = `$True")
         } elseif($Data -match 'False') {
-            Write-Host "False"
             Invoke-Expression ("`$$($Name) = `$False")
         } else {
-            Write-Host "Default"
             New-Variable -Name $Name -Value $Data
         } 
     }
@@ -86,16 +82,12 @@ Do{
     ##PROCESS Doables
     $DoOn = 1,3,5,7,9
     $DoingIndex = Get-Random -Minimum 1 -Maximum $RandPool
-    Write-Host "DO $DoingIndex"
     if($DoOn -contains $DoingIndex){
-        Write-Host "Doing"
         $Index = Get-Random -Maximum 3
-        Write-Host "INDEX $Index"
     
         switch($Index){
             0 {
                 if($CanShowPhoto){
-                    Write-Host "Photos"
                     $Pictures = Get-ChildItem -Path $PhotoFolder | ?{$FileTypes -match $_.Extension}
                     $Rand = Get-Random -Minimum 0 -Maximum ($Pictures.Count - 1)
                     $Picture = $Pictures[$Rand].FullName
@@ -104,7 +96,6 @@ Do{
               }
             1 {
                 if($CanShowVideo){
-                    Write-Host "Video"
                     $Videos = Get-ChildItem -Path $VideoFolder | ?{$FileTypes -match $_.Extension}
                     $Rand = Get-Random -Minimum 0 -Maximum $Videos.Count
                     $Video = $Videos[$Rand].FullName
@@ -113,7 +104,6 @@ Do{
               }
             2 {
                 if($CanShowWeb){
-                    Write-Host "Website"
                     $Rand = Get-Random -Minimum 0 -Maximum $WebLinks.Count
                     $Website = $WebLinks[$Rand]
                     Start-Process -FilePath $Website  
@@ -121,7 +111,6 @@ Do{
               }
         }
     } else {
-        Write-Host "Not Doing"
     }
     
     ##PROCESS UPDATE
@@ -132,7 +121,7 @@ Do{
         Remove-Item $TempFile -Force
         $WC.DownloadFile($UpdatePath,$TempFile)
         if($CurrentVersion -notmatch $NewVersion){
-            Write-Host "Current Version: $CurrentVersion || New Version $NewVersion"
+            Write-Host "Current Version: $CurrentVersion || New Version $NewVersion || Temp File: $TempFile || Update Path: $UpdatePath"
             Write-Host "Copying..."
             Copy-Item -Path $TempFile -Destination $Script -Force
             Remove-Item $TempFile -Force
